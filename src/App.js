eb37e807015c398164e3bcd3fcad4c247c2ac05e
Salman Fazal <rustamas@uni.coventry.ac.uk>
2021-03-18 11:49:28 -0400

    Full project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import SwipeCard from "./components/SwipeCards/SwipeCards";
+import SwipeButtons from "./components/SwipeButtons/SwipeButtons";
+import Chats from "./components/Chats/Chats";
+import ChatScreen from "./components/ChatScreen/ChatScreen";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route path="/chat/:person">
+						<Header backButton="/chat" />
+						<ChatScreen />
+					</Route>
+					<Route path="/chat">
+						<Header backButton="/" />
+						<Chats />
+					</Route>
+					<Route path="/">
+						<Header />
+						<SwipeCard />
+						<SwipeButtons />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
